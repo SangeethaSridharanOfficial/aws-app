68e846c8e389b985828cf1d7c130ab90c5a17e1a
SangeethaSridharan <sangeethasridharan93@gmail.com>
2022-05-25 20:36:22 -0400

    consuming AWS api

---
+++ src/scripts/LoadGalleryImagesData.js
@@ -0,0 +1,33 @@
+var AWS = require("aws-sdk");
+var fs = require('fs');
+
+AWS.config.update({
+  region: "us-east-1"
+});
+
+console.log("Writing entries to GalleryImages table.");
+
+var dynamodb = new AWS.DynamoDB.DocumentClient();
+var galleryImagesData = 
+  JSON.parse(fs.readFileSync('../components/data/gallery_images.json', 'utf8'));
+
+galleryImagesData.forEach((galleryImage) => {
+  var className = galleryImage.className;
+
+  var params = {
+    TableName: "GalleryImages",
+    Item: {
+      "src": galleryImage.src,
+      "alt": galleryImage.alt,
+      "className": className.trim() ? className : 'no_class'
+    }
+  };
+
+  dynamodb.put(params, (err, data) => {
+    if (err)
+      console.error("Unable to load data into table for gallery images",
+                    galleryImage.src, ". Error: ", JSON.stringify(err, null, 2))
+    else
+      console.log("Added", galleryImage.src, "to table.")
+  });
+});
